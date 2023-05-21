b7599d0cb127e9aa9238b9739dea236fb07767f0
Shanto Islam <rajs45693@gmail.com>
2023-05-21 17:18:26 +0600

    implement some issu

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+         client.connect();
 
         // const categoryCollection = client.db("ToyStory").collection("category");
         // const subCategoriesCollection = client.db("ToyStory").collection("categories");
